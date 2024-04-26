c407d8acea082ea94609a1fa8ae2985fd73d5010
Franklin Kong <franklinkong981@gmail.com>
2024-04-26 15:23:55 -0700

    Added starter code and expected tests

---
+++ linked-list.js
@@ -0,0 +1,224 @@
+/** Node: node for a singly linked list. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** LinkedList: chained together nodes. */
+
+class LinkedList {
+  constructor(vals = []) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+
+    for (let val of vals) this.push(val);
+  }
+
+  /** push(val): add new value to end of list. */
+
+  push(val) {
+
+  }
+
+  /** unshift(val): add new value to start of list. */
+
+  unshift(val) {
+
+  }
+
+  /** pop(): return & remove last item. */
+
+  pop() {
+
+  }
+
+  /** shift(): return & remove first item. */
+
+  shift() {
+
+  }
+
+  /** getAt(idx): get val at idx. */
+
+  getAt(idx) {
+
+  }
+
+  /** setAt(idx, val): set val at idx to val */
+
+  setAt(idx, val) {
+
+  }
+
+  /** insertAt(idx, val): add node w/val before idx. */
+
+  insertAt(idx, val) {
+
+  }
+
+  /** removeAt(idx): return & remove item at idx, */
+
+  removeAt(idx) {
+
+  }
+
+  /** average(): return an average of all values in the list */
+
+  average() {
+    
+  }
+}
+
+/*
+describe("push", function() {
+  it("appends node and increments length", function() {
+    let lst = new LinkedList();
+
+    lst.push(5);
+    expect(lst.length).toBe(1);
+    expect(lst.head.val).toBe(5);
+    expect(lst.tail.val).toBe(5);
+
+    lst.push(10);
+    expect(lst.length).toBe(2);
+    expect(lst.head.val).toBe(5);
+    expect(lst.head.next.val).toBe(10);
+    expect(lst.tail.val).toBe(10);
+
+    lst.push(15);
+    expect(lst.length).toBe(3);
+    expect(lst.head.val).toBe(5);
+    expect(lst.head.next.next.val).toBe(15);
+    expect(lst.tail.val).toBe(15);
+  });
+});
+
+describe("unshift", function() {
+  it("adds node at start and increments length", function() {
+    let lst = new LinkedList();
+
+    lst.unshift(5);
+    expect(lst.length).toBe(1);
+    expect(lst.head.val).toBe(5);
+    expect(lst.tail.val).toBe(5);
+
+    lst.unshift(10);
+    expect(lst.length).toBe(2);
+    expect(lst.head.val).toBe(10);
+    expect(lst.head.next.val).toBe(5);
+    expect(lst.tail.val).toBe(5);
+
+    lst.unshift(15);
+    expect(lst.length).toBe(3);
+    expect(lst.head.val).toBe(15);
+    expect(lst.head.next.next.val).toBe(5);
+    expect(lst.tail.val).toBe(5);
+  });
+});
+
+describe("pop", function() {
+  it("removes node at end and decrements length", function() {
+    let lst = new LinkedList([5, 10]);
+
+    expect(lst.pop()).toBe(10);
+    expect(lst.head.val).toBe(5);
+    expect(lst.tail.val).toBe(5);
+    expect(lst.length).toBe(1);
+
+    expect(lst.pop()).toBe(5);
+    expect(lst.tail).toBe(null);
+    expect(lst.head).toBe(null);
+    expect(lst.length).toBe(0);
+  });
+});
+
+describe("shift", function() {
+  it("removes node at start and decrements length", function() {
+    let lst = new LinkedList([5, 10]);
+
+    expect(lst.shift()).toBe(5);
+    expect(lst.tail.val).toBe(10);
+    expect(lst.length).toBe(1);
+
+    expect(lst.shift()).toBe(10);
+    expect(lst.tail).toBe(null);
+    expect(lst.head).toBe(null);
+    expect(lst.length).toBe(0);
+  });
+});
+
+describe("getAt", function() {
+  it("gets val at index", function() {
+    let lst = new LinkedList([5, 10]);
+
+    expect(lst.getAt(0)).toBe(5);
+    expect(lst.getAt(1)).toBe(10);
+  });
+});
+
+describe("setAt", function() {
+  it("sets val at index", function() {
+    let lst = new LinkedList([5, 10]);
+
+    expect(lst.setAt(0, 1));
+    expect(lst.setAt(1, 2));
+    expect(lst.head.val).toBe(1);
+    expect(lst.head.next.val).toBe(2);
+  });
+});
+
+describe("insertAt", function() {
+  it("inserts node and adjusts nearby nodes", function() {
+    let lst = new LinkedList([5, 10, 15, 20]);
+
+    lst.insertAt(2, 12);
+    expect(lst.length).toBe(5);
+    expect(lst.head.val).toBe(5);
+    expect(lst.head.next.val).toBe(10);
+    expect(lst.head.next.next.val).toBe(12);
+    expect(lst.head.next.next.next.val).toBe(15);
+    expect(lst.head.next.next.next.next.val).toBe(20);
+
+    lst.insertAt(5, 25);
+    expect(lst.head.next.next.next.next.next.val).toBe(25);
+    expect(lst.tail.val).toBe(25);
+  });
+
+  it("inserts into empty list", function() {
+    let lst = new LinkedList();
+
+    lst.insertAt(0, 5);
+    expect(lst.length).toBe(1);
+    expect(lst.head.val).toBe(5);
+    expect(lst.tail.val).toBe(5);
+  });
+});
+
+describe("removeAt", function() {
+  it("removes from 1-item list", function() {
+    let lst = new LinkedList(["a"]);
+
+    lst.removeAt(0);
+    expect(lst.length).toBe(0);
+    expect(lst.head).toBe(null);
+    expect(lst.tail).toBe(null);
+  });
+});
+
+describe("average", function() {
+  it("calculates the average of items in a list", function() {
+    let lst = new LinkedList([2, 3, 1, 1, 7, 6, 9]);
+    expect(lst.average()).toBeCloseTo(4.1429, 4);
+  });
+
+  it("returns 0 for empty lists", function() {
+    let lst = new LinkedList();
+    expect(lst.average()).toBe(0);
+  });
+});
+
+*/
